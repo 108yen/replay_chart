e6e6c054d054a5d3ce218839d8b14887ca324a4a
mac <kazuking.1911@gmail.com>
2022-05-02 23:24:43 +0900

    flamegameで動かしてみた 更新間隔の時間管理がめんどい

--- lib/test/flameTest.dart
+++ lib/test/flameTest.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/palette.dart';
@@ -28,17 +30,26 @@ class MyRectangle extends RectangleComponent {
             position: Vector2(x, y),
             size: Vector2(width, height),
             paint: paint);
+  double i = 0;
+  int r = 100;
+  double time = 0;
 
   @override
   void update(double dt) {
     super.update(dt);
+    time += dt;
+    i = time / 2;
+    if (i > 2 * pi) {
+      i = 0;
+      time = 0;
+    }
   }
 
   @override
   void render(Canvas canvas) {
     canvas.drawRect(
       Rect.fromLTWH(
-          position.x, position.y, position.x + size.x, position.y + size.y),
+          position.x + cos(i) * r, position.y + sin(i) * r, size.x, size.y),
       paint,
     );
   }
@@ -47,9 +58,9 @@ class MyRectangle extends RectangleComponent {
 class MyGame extends FlameGame {
   @override
   Future<void> onLoad() async {
-    await add(MyCrate());
+    // await add(MyCrate());
 
-    final paint = BasicPalette.red.paint()..style = PaintingStyle.stroke;
+    final paint = Paint()..color = Colors.red;
     // final square = RectangleComponent.square(
     //   size: 200.0,
     //   position: Vector2(100, 200),
@@ -66,9 +77,9 @@ class MyGame extends FlameGame {
 
     final myRectangle = MyRectangle(
       x: 200,
-      y: 200,
-      width: 200,
-      height: 200,
+      y: 100,
+      width: 20,
+      height: 20,
       paint: paint,
     );
     add(myRectangle);
